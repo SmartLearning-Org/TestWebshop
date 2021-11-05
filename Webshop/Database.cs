33e4a314e5e99bf58df9014c892d4a230a726ab0
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2021-11-05 08:11:01 +0100

    Add project files.

---
+++ Webshop/Database.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop
+{
+    public class Database
+    {
+        public void AddToBasket(string brugernavn, int varenummer, int antal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BasketEntry[] GetBasket(string brugernavn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Article GetArticle(int varenummer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
